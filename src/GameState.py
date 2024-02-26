0207082998775efcfb22619f131e6d6dab402c18
Saemundur <saemiragnars@gmail.com>
2024-02-26 17:56:17 +0100

    overwrite print Move

--- src/GameState.py
+++ src/GameState.py
@@ -1,5 +1,5 @@
 from Player import Player
-import pandas as pd
+from Move import Move
 
 class GameState:
     def __init__(self, player1, player2):
@@ -18,15 +18,29 @@ class GameState:
         self.player1 = player1
         self.player2 = player2
         self.white_to_move = True
-
+        self.move_log = []
 
 
     def make_move(self, move):
         self.board[move.row1][move.col1] = 1
         self.board[move.row2][move.col2] = 2
+        self.white_to_move = not self.white_to_move
+        self.move_log.append(move)
 
     def get_legal_moves(self):
-        pass
+        legal_moves = []
+        for row_white in range(9):
+            for col_white in range(9):
+                # Check if the square is empty
+                if self.board[row_white][col_white] == 0:
+                    # Check if the square is adjacent to a black piece
+                    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+                    for dr, dc in directions:
+                        row_black, col_black = row_white + dr, col_white + dc
+                        # Check if the black piece is within the board and the square is empty
+                        if 0 <= row_black < 9 and 0 <= col_black < 9 and self.board[row_black][col_black] == 0:
+                            legal_moves.append(Move(row_white, col_white, row_black, col_black))
+        return legal_moves
 
     def finished(self):
         # TODO: Rmeove this
--- src/Move.py
+++ src/Move.py
@@ -1,8 +1,18 @@
 class Move:
     def __init__(self, row1, col1, row2, col2):
         # coordinate 1 is white, 2 is black
-        
         self.row1 = row1
         self.col1 = col1
         self.row2 = row2
         self.col2 = col2
+    
+    def __str__(self):
+        # Convert column numbers to letters
+        col1_letter = chr(self.col1 + ord('A'))
+        col2_letter = chr(self.col2 + ord('A'))
+        
+        # Create string representation
+        coord1 = f"{col1_letter}{self.row1}"
+        coord2 = f"{col2_letter}{self.row2}"
+        return f"({coord1} to {coord2})"        
+        
