b497f9fa160672adad240429fed3767e95754b19
Saemundur <saemiragnars@gmail.com>
2024-03-04 14:53:07 +0100

    random bot as player class, not AI class

--- src/Players/AI.py
+++
@@ -1,9 +0,0 @@
-from Move import Move
-from Player import Player
-
-class AI(Player):
-    def get_move(self, gamestate):
-        # Evaluate the board and return the best move
-        pass
-        
-    
--- src/Players/TheDrunkenFool.py
+++ src/Players/TheDrunkenFool.py
@@ -1,7 +1,7 @@
 import random
-from AI import AI
+from Players import Player
 
-class TheDrunkenFool(AI):
+class TheDrunkenFool(Player):
     def get_move(self, gamestate):
         legal_moves = gamestate.get_legal_moves()
         return random.choice(legal_moves)
